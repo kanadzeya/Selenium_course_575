417f00d4d03b9e6bc8dc044deb23cb908c3ce830
NadyaK <nanadin@mail.ru>
2026-03-11 09:59:18 +0300

    Conftest.py sent to repository

---
+++ conftest.py
@@ -0,0 +1,39 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+
+
+def pytest_addoption(parser):
+    # Добавляем параметры в командную строку
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: ru, en, es, fr, etc.")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = ChromeOptions()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        options = FirefoxOptions()
+        options.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(options=options)
+
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+
+    # Неявное ожидание для подстраховки при долгой загрузке
+    browser.implicitly_wait(10)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
